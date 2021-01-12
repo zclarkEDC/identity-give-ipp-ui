e22a37940342757841f3ba77e1e76c11a24cd29e
zclarkED <zclark@easydynamics.com>
2021-01-12 09:50:37 -0500

    update App.js routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import DetailsPage from './components/DetailsPage';
+import EmailPage from './components/EmailPage';
+import ZipForm from './components/ZipForm'
+//import ScrollToTop from './containers/ScrollToTop' place the <ScrollToTop/> component below BrowserRouter, this seems to make the UI screen transations more abrupt
+class App extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <div>
+                    <Switch>
+                        <Route exact path="/">
+                            <ZipForm />
+                        </Route>
+                        <Route path="/details" component={DetailsPage}>
+                        </Route>
+                        <Route path="/email_confirmation" component={EmailPage}>
+                        </Route>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
