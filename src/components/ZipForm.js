ba7f6703a2afb0910b7b6e4abbcad0ea1a25bdb1
zclarkED <zclark@easydynamics.com>
2021-01-15 13:03:45 -0500

    update ZipForm.js to include postalcode validation

--- src/components/ZipForm.js
+++ src/components/ZipForm.js
@@ -1,5 +1,7 @@
+/* old first PR
 import React from 'react';
 import Results from './Results'
+import { postcodeValidator, postcodeValidatorExistsForCountry } from 'postcode-validator';
 
 class ZipForm extends React.Component {
     constructor(props) {
@@ -15,6 +17,10 @@ class ZipForm extends React.Component {
   
     handleSubmit(event) {
       event.preventDefault();
+      let zip_input = this.state.value;
+      if (postcodeValidator(zip_input, 'US')){
+        alert('Valid zipcode, beep boop');
+      }
       this.setState({showResults: true});
       this.setState({submittedval: this.state.value});
       
@@ -27,9 +33,6 @@ class ZipForm extends React.Component {
 
         <div class="container">
             <div class="px2 py2 sm-py5 sm-px6 mx-auto sm-mb5 border-box card" role="main">
-
-
-
 
                 <h1 class="h3 my0">
                     Verify your identity in person
@@ -62,6 +65,186 @@ class ZipForm extends React.Component {
                 <div class="mt2 pt1 border-top">
                     <a href="https://secure.login.gov/">Cancel</a>
                 </div>
+            </div>
+        </div>
+
+        
+      );
+    }
+  }
+  
+  export default ZipForm;
+  
+ */
+
+//working with valid zip
+/*
+import React from 'react';
+import Results from './Results';
+import { postcodeValidator, postcodeValidatorExistsForCountry } from 'postcode-validator';
+import ZipInput from './ZipInput';
+import Button from './Button';
+
+class ZipForm extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {value: '', showResults: false, submittedval: '',};
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+  
+    handleSubmit(event) {
+      event.preventDefault();
+      let zip_input = this.state.value;
+      if (postcodeValidator(zip_input, 'US')){
+        alert('Valid zipcode, beep boop');
+      }
+      this.setState({showResults: true});
+      this.setState({submittedval: this.state.value});
+      
+    }
+    
+    render() {
+        
+      return (
+
+
+        <div class="container">
+            <div class="px2 py2 sm-py5 sm-px6 mx-auto sm-mb5 border-box card" role="main">
+
+                <h1 class="h3 my0">
+                    Verify your identity in person
+                </h1>
+                <br></br>
+                <p class='mt-tiny mb0'>
+                    You will need to stop by an identity services center to finish verifying your identity.
+                </p>
+                <div class="mb4">
+              <form class="simple_form mt3 sm-mt3 new_user" onSubmit={this.handleSubmit} accept-charset="UTF-8">
+                          
+              <ZipInput
+              name="zip"
+              type="text"
+              value={this.state.value}
+              onChange={this.handleChange}
+              required="required"
+              className="block col-12 field string zip required"
+              aria-invalid="false"
+              aria-required="true"
+              id="zip"
+            />
+            <Button
+              type="submit"
+              value="Submit"
+              label="Search"
+              className="btn btn-primary btn-wide"
+              
+            />
+            { this.state.showResults ? <Results item={this.state.submittedval}/> : null }
+            </form>
+            </div>
+            <div class="mt2 pt1 border-top">
+                    <a href="https://secure.login.gov/">Cancel</a>
+                </div>
+                
+
+            </div>
+        </div>
+
+        
+      );
+    }
+  }
+  
+  export default ZipForm;
+*/
+
+import React from 'react';
+import Results from './Results';
+import { postcodeValidator} from 'postcode-validator';
+import ZipInput from './ZipInput';
+import Button from './Button';
+import Error from './Error'
+
+class ZipForm extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {value: '', showResults: false, submittedval: '', showError: false};
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+  
+    handleSubmit(event) {
+      event.preventDefault();
+      let zip_input = this.state.value;
+      if (postcodeValidator(zip_input, 'US')){
+        //alert('Valid zipcode, beep boop');
+        this.setState({showResults: true});
+        this.setState({showError: false});
+        this.setState({submittedval: this.state.value});
+      }
+      else{
+        //alert('Invalid zipcode, beep boop');
+        this.setState({showResults: false});
+        this.setState({showError: true});
+        this.setState({submittedval: this.state.value});
+      }
+      
+    }
+    
+    render() {
+        
+      return (
+
+
+        <div class="container">
+            <div class="px2 py2 sm-py5 sm-px6 mx-auto sm-mb5 border-box card" role="main">
+
+                <h1 class="h3 my0">
+                    Verify your identity in person
+                </h1>
+                <br></br>
+                <p class='mt-tiny mb0'>
+                    You will need to stop by an identity services center to finish verifying your identity.
+                </p>
+                <div class="mb4">
+              <form class="simple_form mt3 sm-mt3 new_user" onSubmit={this.handleSubmit} accept-charset="UTF-8">
+                          
+              <ZipInput
+              name="zip"
+              type="text"
+              value={this.state.value}
+              onChange={this.handleChange}
+              required="required"
+              className="block col-12 field string zip required"
+              aria-invalid="false"
+              aria-required="true"
+              id="zip"
+            />
+            <Button
+              type="submit"
+              value="Submit"
+              label="Search"
+              className="btn btn-primary btn-wide"
+              
+            />
+            { this.state.showResults ? <Results item={this.state.submittedval}/> : null }
+            { this.state.showError ? <Error item={this.state.submittedval}/> : null }
+            </form>
+            </div>
+            <div class="mt2 pt1 border-top">
+                    <a href="https://secure.login.gov/">Cancel</a>
+                </div>
+                
+
             </div>
         </div>
 
