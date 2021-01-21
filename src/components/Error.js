d6f97e1e526a9c4822b6149bedf4947a567e2546
zclarkED <zclark@easydynamics.com>
2021-01-21 11:58:37 -0500

    clean up comments in Error.js

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,31 +1,3 @@
-/*
-import React from 'react';
-
-class Error extends React.Component {
-
-    constructor(props) {
-        super(props);
-   
-    }
-    render() {
-
-        return (
-            <div>
-                <br></br>
-                <br></br>
-                <p class='mt-tiny mb0'>
-                    Error! {this.props.item} is not a valid zip code!
-                </p>
-            </div>
-
-        );
-    }
-}
-export default Error;
-
-
-*/
-
 import React from 'react';
 
 class Error extends React.Component {
@@ -49,9 +21,3 @@ class Error extends React.Component {
     }
 }
 export default Error;
-/*
-<div class="usa-alert usa-alert--error margin-bottom-4" role="alert">
-            <div class="usa-alert__body">
-            <p class="usa-alert__text">The email or password you've entered is wrong. Try resetting your password.</p>
-            </div>
-            </div>*/
