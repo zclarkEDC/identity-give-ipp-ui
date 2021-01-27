7b4775175e23ae1d7cef561154c1264e67716750
zclarkED <zclark@easydynamics.com>
2021-01-27 15:21:08 -0500

    create MapResults.js component, used to manage the map boundbox and maplocations

---
+++ src/components/MapResults.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import MapLocation from './MapLocation'
+class MapResults extends Component {
+
+    render() {
+        const zipitemBounds = [
+            [this.props.zipitem.boundingbox[0], this.props.zipitem.boundingbox[2]],
+            [this.props.zipitem.boundingbox[1], this.props.zipitem.boundingbox[3]]
+        ]
+        return (
+
+            <div>
+                 <p class='mt-tiny mb0'>Results near {this.props.zipitem.display_name}</p>
+                 <p>{this.props.zipitem.boundingbox[0]}</p>
+                 <p>{this.props.zipitem.boundingbox[1]}</p>
+                 <p>{this.props.zipitem.boundingbox[2]}</p>
+                 <p>{this.props.zipitem.boundingbox[3]}</p>
+                 <MapContainer bounds={zipitemBounds} zoom={13} scrollWheelZoom={true}>
+          <TileLayer
+            attribution='hi'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {this.props.coords.map((item) =>
+          <MapLocation key={item.id} item={item} />)}
+        </MapContainer>
+                 
+            </div>
+        );
+    }
+}
+
+export default MapResults;
