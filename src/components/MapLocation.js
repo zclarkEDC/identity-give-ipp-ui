b83d10ba468bcd06c86caf441faf4587ba1f79f7
zclarkED <zclark@easydynamics.com>
2021-01-28 22:20:06 -0500

    update MapLocation to use new props from actual ipp/locations endpoint

--- src/components/MapLocation.js
+++ src/components/MapLocation.js
@@ -4,13 +4,15 @@ import { Marker, Popup } from 'react-leaflet'
 class MapLocation extends Component {
 
     render() {
-        let x = this.props.item.address.geo.lat
-        let y = this.props.item.address.geo.lng
+        let x = this.props.item.geocode.latitude
+        let y = this.props.item.geocode.longitude
         return (
 
             <div>
                 <Marker position={[x,y]}>
-                    <Popup>{this.props.item.email}<br /> Easily customizable.</Popup>
+                    <Popup> <h1 class="h3 my0" >
+            Verify your identity in person
+                </h1>{this.props.item.title}<br /> Easily customizable.</Popup>
                 </Marker>
 
             </div>
