a26ced4eecf93e0db750b196ea77b7ff36b45501
zclarkED <zclark@easydynamics.com>
2021-01-28 22:18:44 -0500

    create method to authenticate with KONG

--- src/components/ZipForm.js
+++ src/components/ZipForm.js
@@ -8,26 +8,64 @@ import axios from 'axios';
 
 
 
+
 class ZipForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: '', showResults: false, submittedval: '', showError: false, items: [], zipresponse: [] };
+    this.state = { value: '', showResults: false, submittedval: '', showError: false, items: [], zipresponse: [], authtoken: {} };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  //Automatically make call for authentication when page loads
+  componentDidMount() {
+    //console.log('starting authentication');
+
+    let config = {
+      method: 'post',
+      url: 'https://give-dev.app.cloud.gov/auth/oauth2/token',
+      data: {
+        "client_id": "AusOWjdsOJDyTHSRBsX4gVo9Ge09t9Rc",
+        "client_secret": "tAIKqzqkEHjYfF4kSFomPwmCLAwzCn73",
+        "grant_type": "client_credentials",
+        "scope": "rpname",
+        "provision_key": "gJHRiS8aCrYVqrdAaRFeoC1zAm7vmOW2",
+        "authenticated_userid": "login-demo"
+      },
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Request-Methods': 'GET, POST, OPTIONS',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Credentials': true
+      }
+
+    }
+
+    axios(config)
+      .then((response) => {
+        //console.log('here is the KONG response', JSON.stringify(response.data));
+        this.setState({ authtoken: JSON.parse(JSON.stringify(response.data)) })
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   handleChange(event) {
     this.setState({ value: event.target.value });
+    //console.log('inside handle change, checking moth props', event.target.)
+
   }
 
   handleSubmit(event) {
     event.preventDefault();
     let zip_input = this.state.value;
+
     if (postcodeValidator(zip_input, 'US')) {
       //alert('Valid zipcode, beep boop');
       this.setState({ submittedval: this.state.value });
       this.fetchAddressFromZip(this.state.value);
-      
+
     }
     else {
       //alert('Invalid zipcode, beep boop');
@@ -38,15 +76,25 @@ class ZipForm extends React.Component {
 
   }
   fetchData = async values => {
-    const { value } = values; //placeholder for API calls made with the actual zipcode
+    //const { value } = values; //placeholder for API calls made with the actual zipcode
+    var access_token = JSON.stringify(this.state.authtoken.access_token);
+    access_token = access_token.replace(/^"(.+(?="$))"$/, '$1');
+    var bearer = 'bearer '
+    let auth = bearer.concat(access_token);
+    var url = "https://give-dev.app.cloud.gov/ipp/locations/";
+    var zipsearch = url.concat(values)
+
     let config = {
       method: 'get',
-      url: "https://jsonplaceholder.typicode.com/users"
-      //data: values this is where the zip info would be placed
+      url: zipsearch,
+      headers: {
+        'Authorization': auth
+      }
     };
+    //console.log(config);
     axios(config)
       .then((response) => {
-        console.log(JSON.stringify(response.data));
+        //console.log(JSON.stringify(response.data));
         this.setState({ items: JSON.parse(JSON.stringify(response.data)) })
       })
       .catch((error) => {
@@ -57,8 +105,7 @@ class ZipForm extends React.Component {
 
   //This method makes a call to OSM to determine the address based on the Zipcode
   fetchAddressFromZip = async zipcode => {
-    console.log('zipcode going in');
-    console.log(zipcode);
+    //console.log(zipcode);
     let config = {
       method: 'get',
       url: 'https://nominatim.openstreetmap.org/search',
@@ -70,13 +117,12 @@ class ZipForm extends React.Component {
     }
     axios(config)
       .then((response) => {
-        console.log(JSON.stringify(response.data));
+        //console.log(JSON.stringify(response.data));
         this.setState({ zipresponse: JSON.parse(JSON.stringify(response.data)) })
-        console.log(this.state.zipresponse)
 
         //Validate if OSM returned any location results
         if (this.state.zipresponse?.length) {
-          console.log('this array has stuff in it ok? here it is:', this.state.zipresponse)
+          //console.log('this array has stuff in it ok? here it is:', this.state.zipresponse)
           this.fetchData(zipcode);
           this.setState({ showResults: true });
           this.setState({ showError: false });
@@ -87,17 +133,15 @@ class ZipForm extends React.Component {
           console.log('no results found, empty array', this.state.zipresponse)
           this.setState({ showResults: false });
           this.setState({ showError: true });
-          
+
         }
       })
       .catch((error) => {
         console.log(error);
       });
   }
-
-
   render() {
-    
+
     return (
 
       <div class="container">
@@ -105,7 +149,7 @@ class ZipForm extends React.Component {
 
           {this.state.showError ? <Error item={this.state.submittedval} /> : null}
 
-          <h1 class="h3 my0">
+          <h1 class="h3 my0" >
             Verify your identity in person
                 </h1>
           <br></br>
@@ -138,9 +182,7 @@ class ZipForm extends React.Component {
             </form>
           </div>
           <div class="mt2 pt1 border-top">
-            <a href="https://secure.login.gov/">Cancel</a>
-          </div>
-
+            <a href="https://secure.login.gov/">Cancel</a></div>
         </div>
       </div>
 
