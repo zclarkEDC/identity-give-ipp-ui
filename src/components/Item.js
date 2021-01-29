50a6131557a625ec62c6e1f4eaf18fe9e8eeb2db
zclarkED <zclark@easydynamics.com>
2021-01-28 22:21:01 -0500

    update Item component to use actual ipp/locations props

--- src/components/Item.js
+++ src/components/Item.js
@@ -7,25 +7,26 @@ class Item extends Component {
         return (
 
             <div>
-                <h2>{this.props.item.email}</h2>
+                <hr></hr>
+                <h2>{this.props.item.title}</h2>
 
                 <div class="grid-row">
                     <div class="grid-col-8">
-                        <p>{this.props.item.address.street}<br />{this.props.item.address.city}, {this.props.item.address.state} {this.props.item.address.zipcode}<br /><a href="url">{this.props.item.phone}</a></p>
+                        <p>{this.props.item.address}<br />{this.props.item.city}, {this.props.item.state} {this.props.item.postalCode}<br /><a href="url">{this.props.item.phone}</a></p>
                     </div>
                     <div class="grid-col-2"></div>
-                    <div class="grid-col-2 text-right"><b>10 mi</b></div>
+                    <div class="grid-col-2 text-right"><b>{Math.round(this.props.item.distance)} mi</b></div>
                 </div>
 
 
                 <Link to={{
                     pathname: '/details',
-                    state: { targetItem: this.props.item.email, itema: this.props.item.address.street, itemb: this.props.item.address.city, itemc: this.props.item.address.state, itemd: this.props.item.address.zipcode, iteme: this.props.item.phone }
+                    state: { title: this.props.item.title, itema: this.props.item.address, itemb: this.props.item.city, itemc: this.props.item.state, itemd: this.props.item.postalCode, iteme: this.props.item.phone }
 
                 }}><div class="grid-row"><input type="submit" name="submit"
                     class="btn btn-primary btn-block mt0" value="Select this branch" data-disable-with="Select this branch" /></div></Link>
                 <br />
-                <hr></hr>
+                
             </div>
         );
     }
