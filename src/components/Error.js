ea19664c9ca27e7d55a20d3db8bd3a87f75514b5
zclarkED <zclark@easydynamics.com>
2021-02-01 16:54:27 -0500

    update error token

--- src/components/Error.js
+++ src/components/Error.js
@@ -12,7 +12,7 @@ class Error extends React.Component {
             <div class="usa-alert usa-alert--error margin-bottom-4" role="alert">
                <div class="usa-alert__body">
                <p class="usa-alert__text">
-                    Error! {this.props.item} is not a valid zip code!
+                    Error! {this.props.item} is not a valid zip code! {this.props.message}
                 </p>
             </div>
             </div>
