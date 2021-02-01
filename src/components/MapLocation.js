374c5ffc64a82003b6d8f960b6f41f08d3cfd4d5
zclarkED <zclark@easydynamics.com>
2021-02-01 09:38:32 -0500

    fix MapLocation title to properly render TM

--- src/components/MapLocation.js
+++ src/components/MapLocation.js
@@ -4,13 +4,15 @@ import { Marker, Popup } from 'react-leaflet'
 class MapLocation extends Component {
 
     render() {
+        var oldtitle = this.props.item.title;
+        var fixedtitle = oldtitle.replace('&#8482', '\u2122');
         let x = this.props.item.geocode.latitude
         let y = this.props.item.geocode.longitude
         return (
 
             <div>
                 <Marker position={[x,y]}>
-                    <Popup> <h6>{this.props.item.title}</h6><p>{this.props.item.address}<br />{this.props.item.city}, {this.props.item.state} {this.props.item.postalCode}<br /><a href="url">{this.props.item.phone}</a></p></Popup>
+                    <Popup> <h6>{fixedtitle}</h6><p>{this.props.item.address}<br />{this.props.item.city}, {this.props.item.state} {this.props.item.postalCode}<br /><a href="url">{this.props.item.phone}</a></p></Popup>
                 </Marker>
 
             </div>
