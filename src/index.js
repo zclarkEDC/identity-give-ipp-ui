a994a071f14bffa74f9fe17d7cb86bbe9bd3124a
zclarkED <zclark@easydynamics.com>
2021-02-02 14:32:15 -0500

    update app rendering with index.js

--- src/index.js
+++ src/index.js
@@ -8,14 +8,17 @@ import 'identity-style-guide/dist/assets/js/main.js'
 import '../node_modules/identity-style-guide/dist/assets/js/main.js'
 import 'uswds/src/js/polyfills';
 import { accordion } from 'identity-style-guide';
+import Banner from './components/SiteBanner';
+import Logo from './components/SiteLogo';
+
 accordion.on();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div><div class="site-wrap bg-light-blue"><Banner/><Logo/><App/></div></div>,
   document.getElementById('root')
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
