3cef55fd8df2c742167c24f4f60bd0bf49f3aa68
zclarkED <zclark@easydynamics.com>
2021-02-05 15:10:51 -0500

    clean Details.js of comments

--- src/components/Details.js
+++ src/components/Details.js
@@ -2,29 +2,15 @@ import React, { Component } from 'react';
 import {Link } from 'react-router-dom';
 
 class Details extends Component {
-
-    componentDidMount(){
-        console.log("the target item is", this.props.location.state.targetItem)
-    }
+    
     render() {
         return (
-            
-                
                 <div class="grid-row">
                     <h4>{this.props.location.state.title}</h4>
                     <div class="grid-col-8">
                         {this.props.location.state.itema}<br/>
-                        {this.props.location.state.itemb}, {this.props.location.state.itemc} {this.props.location.state.itemd}<br/><a
-                            >{this.props.location.state.iteme}</a>
-
-                    </div>
+                        {this.props.location.state.itemb}, {this.props.location.state.itemc} {this.props.location.state.itemd}<br/><a>{this.props.location.state.iteme}</a></div>
                     <div class="grid-col-1"></div>
                     <div class="grid-col-3"><br/><Link to="/"><a >Change Branch</a></Link></div>
-                </div>
-            
-
-        );
-    }
-}
-
+                </div>);}}
 export default Details;
